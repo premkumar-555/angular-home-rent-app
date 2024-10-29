74bc239480b3c6cc828cd880eef56d45e8f4a71d
premkumar-555 <premkumar@moneyone.com>
2024-10-29 18:28:42 +0530

    created interface for Housinglocation

--- first-app-lesson-00/src/app/home/home.component.ts
+++ first-app-lesson-00/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Housinglocation } from './../housinglocation';
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
@@ -20,5 +21,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
